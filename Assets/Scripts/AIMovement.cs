3535fe755a131eb1ee6fd305a0e73e021b79def4
nbaines <nbaines@umich.edu>
2021-12-25 21:00:02 -0500

    Pushing the shareable project up to github

---
+++ Assets/Scripts/AIMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour {
+
+	Animator anim;
+	NavMeshAgent agent;
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		anim = GetComponent<Animator>();
+		player = GameObject.Find("Lumberjack1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if  (player.transform.position.y > 19f && 
+			(player.transform.position.x < 53f && player.transform.position.x > 8f) && 
+			(player.transform.position.z < 58f && player.transform.position.z > 29f) && Vector3.Distance(player.transform.position, transform.position) > 1.5f)
+			{
+				
+				anim.SetBool("isWalking", true);
+				agent.SetDestination(player.transform.position);
+				
+			}
+		else
+		{
+			anim.SetBool("isWalking", false);
+			agent.SetDestination(transform.position);
+		}
+		if (Vector3.Distance(player.transform.position, transform.position) < 1.5f) // if he's in range, skeleton swings his weapon
+			anim.SetBool("inRange", true);
+		else
+			anim.SetBool("inRange", false);
+		
+	}
+}
---
+++ Assets/Scripts/RestartScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class RestartScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("attempting to restart scene");
+		SceneManager.LoadScene("SampleScene");
+	}
+}
